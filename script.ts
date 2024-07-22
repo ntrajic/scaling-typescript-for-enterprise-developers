ae5f42cfdf7d94fe54feecd4894af84bc82c7f3e
eveporcello <eve@moonhighway.com>
2024-07-22 16:41:52 -0400

    Adds capstone start

--- script.ts
+++ script.ts
@@ -1,14 +1,1 @@
-class City {
-  constructor(public name: string) {}
-}
-
-function outputLocation(location: City | string) {
-  if (location instanceof City) {
-    console.log(location.name);
-  } else {
-    console.log(location);
-  }
-}
-
-outputLocation(new City("Copenhagen"));
-outputLocation("Boise");
+// Create your class here.
