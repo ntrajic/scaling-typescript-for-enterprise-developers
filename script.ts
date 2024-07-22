22b33b95d391e98a9ce86f96fd666aadf174af44
eveporcello <eve@moonhighway.com>
2024-07-22 16:30:42 -0400

    Adds 01_01e

--- script.ts
+++ script.ts
@@ -1,1 +1,13 @@
-console.log("We're ready!");
+class City {
+  name: string;
+  country: string;
+
+  constructor(name: string, country: string) {
+    this.name = name;
+    this.country = country;
+  }
+}
+
+const berlin = new City("Berlin", "Germany");
+console.log(berlin.name);
+console.log(berlin.country);
