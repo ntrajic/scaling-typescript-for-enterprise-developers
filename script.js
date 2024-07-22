bbefdfe82b69830fe467799fb46aa98fc45d9a52
eveporcello <eve@moonhighway.com>
2024-07-22 16:47:06 -0400

    Adds 02_01b

--- script.js
+++ script.js
@@ -1,20 +1,14 @@
-var Hotel = /** @class */ (function () {
-    function Hotel(id, name, cost) {
-        this.amenities = [];
-        this.id = id;
-        this.name = name;
-        this.cost = cost;
-    }
-    Hotel.prototype.addAmenity = function (amenity) {
-        this.amenities.push(amenity);
-    };
-    Hotel.prototype.describeHotel = function () {
-        return "The ".concat(this.name, " costs $").concat(this.cost, " and includes the following amenities: ").concat(this.amenities.join(", "), ".");
-    };
-    return Hotel;
-}());
-var peakLodge = new Hotel("06", "Peak Lodge", 250);
-peakLodge.addAmenity("breakfast");
-peakLodge.addAmenity("wifi");
-var description = peakLodge.describeHotel();
-console.log(description);
+var city1 = {
+    name: "Istanbul",
+    country: "Turkey"
+};
+var city2 = {
+    name: "Amsterdam",
+    country: "Netherlands"
+};
+var venice = {
+    name: "Venice",
+    country: "Italy",
+    nickname: "The Floating City"
+};
+console.log(venice);
