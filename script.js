c2f827e876143aa56ee4ba37693a4f064783d424
eveporcello <eve@moonhighway.com>
2024-07-22 20:27:06 -0400

    Adds 03_01b

--- script.js
+++ script.js
@@ -1,21 +1,5 @@
-var Hotel = /** @class */ (function () {
-    function Hotel(id, name, cost, category) {
-        this.amenities = [];
-        this.id = id;
-        this.name = name;
-        this.cost = cost;
-        this.category = category;
-    }
-    Hotel.prototype.addAmenity = function (amenity) {
-        this.amenities.push(amenity);
-    };
-    Hotel.prototype.describeHotel = function () {
-        return "The ".concat(this.category, " category ").concat(this.name, " costs $").concat(this.cost, " and includes the following amenities: ").concat(this.amenities.join(", "), ".");
-    };
-    return Hotel;
-}());
-var peakLodge = new Hotel("06", "Peak Lodge", 250, "gold");
-peakLodge.addAmenity("breakfast");
-peakLodge.addAmenity("wifi");
-var description = peakLodge.describeHotel();
-console.log(description);
+var cityProperties = [
+    "name",
+    "country",
+    "population"
+];
--- script.ts
+++ script.ts
@@ -1,45 +1,15 @@
-type HotelCategory = "gold" | "silver" | "bronze";
-
-class Hotel {
-  readonly id: string;
-  readonly name: string;
-  cost: number;
-  amenities: string[] = [];
-  category: HotelCategory;
-
-  constructor(
-    id: string,
-    name: string,
-    cost: number,
-    category: HotelCategory
-  ) {
-    this.id = id;
-    this.name = name;
-    this.cost = cost;
-    this.category = category;
-  }
-
-  addAmenity(amenity: string) {
-    this.amenities.push(amenity);
-  }
-  describeHotel(): string {
-    return `The ${this.category} category ${
-      this.name
-    } costs $${
-      this.cost
-    } and includes the following amenities: ${this.amenities.join(
-      ", "
-    )}.`;
-  }
+interface City {
+  name: string;
+  country: string;
+  population: number;
 }
 
-const peakLodge = new Hotel(
-  "06",
-  "Peak Lodge",
-  250,
-  "silver"
-);
-peakLodge.addAmenity("breakfast");
-peakLodge.addAmenity("wifi");
-let description = peakLodge.describeHotel();
-console.log(description);
+type CityProperties = keyof City;
+
+type CityProps = "name" | "city";
+
+const cityProperties: CityProperties[] = [
+  "name",
+  "country",
+  "population"
+];
