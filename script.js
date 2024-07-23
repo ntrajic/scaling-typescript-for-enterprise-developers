061b5550b0942161ba399b955abeb36d44884fe4
eveporcello <eve@moonhighway.com>
2024-07-22 21:00:36 -0400

    Adds 05_06e

--- script.js
+++ script.js
@@ -1,40 +1,9 @@
-var skiLesson = {
-    name: "Steeps Clinic",
-    location: "Matterhorn Gondola",
-    attendees: [
-        {
-            name: "Jessica Sweet",
-            interest: "skiing"
-        },
-        {
-            name: "Jason Williams",
-            interest: "adrenaline sports"
-        }
-    ]
-};
-var cookingClass = {
-    name: "Thai Cooking Class",
-    location: "West Kitchen",
-    attendees: [
-        {
-            name: "Leon Vida",
-            interest: "restaurants"
-        }
-    ]
-};
-var massage = {
-    name: "Hot Stone Massage",
-    location: "Lotus Spa Lounge",
-    attendees: [
-        {
-            name: "Jordan James",
-            interest: "spas"
-        }
-    ]
-};
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 var Hotel = /** @class */ (function () {
     function Hotel(id, name, cost, category) {
         this.amenities = [];
+        this.activities = [];
         this.id = id;
         this.name = name;
         this.cost = cost;
@@ -46,10 +15,21 @@ var Hotel = /** @class */ (function () {
     Hotel.prototype.describeHotel = function () {
         return "The ".concat(this.category, " category ").concat(this.name, " costs $").concat(this.cost, " and includes the following amenities: ").concat(this.amenities.join(", "), ".");
     };
+    Hotel.prototype.addActivity = function (activity) {
+        this.activities.push(activity);
+    };
     return Hotel;
 }());
 var peakLodge = new Hotel("06", "Peak Lodge", 250, "silver");
+var hiking = {
+    name: "Eastlake Summit Hike",
+    location: "Eastlake Trailhead",
+    attendees: [
+        { name: "John Wilson", interest: "adrenaline sports" }
+    ]
+};
 peakLodge.addAmenity("breakfast");
 peakLodge.addAmenity("wifi");
+peakLodge.addActivity(hiking);
 var description = peakLodge.describeHotel();
 console.log(description);
