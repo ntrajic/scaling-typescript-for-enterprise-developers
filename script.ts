9bbc6daebedeb3e7b8f7c063c2ce47d3a3884632
eveporcello <eve@moonhighway.com>
2024-07-22 20:35:57 -0400

    Adds 03_05e

--- script.ts
+++ script.ts
@@ -1,10 +1,24 @@
-const oakData = {
+type Tree = {
+  name: string;
+  height: number;
+  age: number;
+};
+
+const oakData: Tree = {
   name: "Oak",
   height: 20,
   age: 100
 };
 
-let oak = {
+type TreeDetails<Type> = {
+  [Key in keyof Type as `get${Capitalize<
+    string & Key
+  >}`]: () => Type[Key];
+};
+
+type OakTree = TreeDetails<Tree>;
+
+let oak: OakTree = {
   getName: () => oakData.name,
   getHeight: () => oakData.height,
   getAge: () => oakData.age
