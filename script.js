45223fa2b286d854444f00f754fdb4669712de3a
eveporcello <eve@moonhighway.com>
2024-07-22 20:35:20 -0400

    Adds 03_05b

--- script.js
+++ script.js
@@ -1,31 +1,13 @@
-var __assign = (this && this.__assign) || function () {
-    __assign = Object.assign || function(t) {
-        for (var s, i = 1, n = arguments.length; i < n; i++) {
-            s = arguments[i];
-            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
-                t[p] = s[p];
-        }
-        return t;
-    };
-    return __assign.apply(this, arguments);
+var oakData = {
+    name: "Oak",
+    height: 20,
+    age: 100
 };
-var invalidAirports = [
-    { name: "Heathrow", code: "lhr", country: "UK" },
-    { name: "JFK", code: "jfk", country: "USA" },
-    { name: "Schiphol", code: "ams", country: "Netherlands" },
-    { name: "Changi", code: "sin", country: "Singapore" },
-    { name: "Dubai", code: "dxb", country: "UAE" },
-    { name: "Los Angeles", code: "lax", country: "USA" }
-];
-invalidAirports.push({
-    name: "Frankfurt",
-    code: "fra",
-    country: "Germany"
-});
-function transformData(data, transform) {
-    return data.map(transform);
-}
-var airports = transformData(invalidAirports, function (airport) {
-    return __assign(__assign({}, airport), { code: airport.code.toUpperCase(), original: airport });
-});
-console.table(airports);
+var oak = {
+    getName: function () { return oakData.name; },
+    getHeight: function () { return oakData.height; },
+    getAge: function () { return oakData.age; }
+};
+console.log("Name: ".concat(oak.getName()));
+console.log("Height: ".concat(oak.getHeight()));
+console.log("Age: ".concat(oak.getAge()));
--- script.ts
+++ script.ts
@@ -1,71 +1,15 @@
-interface Civilization<NotablePeopleType> {
-  name: string;
-  location: string;
-  notablePeople: NotablePeopleType[];
-}
-
-interface Person {
-  name: string;
-  occupation: string;
-}
-
-type Architect = Person & { occupation: "Architect" };
-type Pharaoh = Person & { occupation: "Pharaoh" };
-type Poet = Person & { occupation: "Poet" };
-type Philosopher = Person & { occupation: "Philosopher" };
-type General = Person & { occupation: "General" };
-
-type NotablePeople<PersonType> = PersonType extends Person
-  ? PersonType
-  : never;
-
-const egyptianCivilization: Civilization<
-  NotablePeople<Architect | Pharaoh>
-> = {
-  name: "Egyptian",
-  location: "Africa",
-  notablePeople: [
-    {
-      name: "Cleopatra",
-      occupation: "Pharaoh"
-    },
-    {
-      name: "Imhotep",
-      occupation: "Architect"
-    }
-  ]
+const oakData = {
+  name: "Oak",
+  height: 20,
+  age: 100
 };
 
-const greekCivilization: Civilization<
-  NotablePeople<Poet | Philosopher>
-> = {
-  name: "Greek",
-  location: "Europe",
-  notablePeople: [
-    {
-      name: "Homer",
-      occupation: "Poet"
-    },
-    {
-      name: "Socrates",
-      occupation: "Philosopher"
-    }
-  ]
+let oak = {
+  getName: () => oakData.name,
+  getHeight: () => oakData.height,
+  getAge: () => oakData.age
 };
 
-const romanCivilization: Civilization<
-  NotablePeople<General | Poet>
-> = {
-  name: "Roman",
-  location: "Europe",
-  notablePeople: [
-    {
-      name: "Julius Caesar",
-      occupation: "General"
-    },
-    {
-      name: "Virgil",
-      occupation: "Poet"
-    }
-  ]
-};
+console.log(`Name: ${oak.getName()}`);
+console.log(`Height: ${oak.getHeight()}`);
+console.log(`Age: ${oak.getAge()}`);
