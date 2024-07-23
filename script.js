1c2dbeb1b44f12e9701a2f3cf45b4d8e166a1ffb
eveporcello <eve@moonhighway.com>
2024-07-22 20:49:50 -0400

    Adds 04_05e

--- script.js
+++ script.js
@@ -1,37 +1,3 @@
-var skiLesson = {
-    name: "Steeps Clinic",
-    location: "Matterhorn Gondola",
-    attendees: [
-        {
-            name: "Jessica Sweet",
-            interest: "skiing"
-        },
-        {
-            name: "Jason Williams",
-            interest: "adrenaline sports"
-        }
-    ]
-};
-var cookingClass = {
-    name: "Thai Cooking Class",
-    location: "West Kitchen",
-    attendees: [
-        {
-            name: "Leon Vida",
-            interest: "restaurants"
-        }
-    ]
-};
-var massage = {
-    name: "Hot Stone Massage",
-    location: "Lotus Spa Lounge",
-    attendees: [
-        {
-            name: "Jordan James",
-            interest: "spas"
-        }
-    ]
-};
 var Hotel = /** @class */ (function () {
     function Hotel(id, name, cost, category) {
         this.amenities = [];
