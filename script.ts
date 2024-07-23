c2a4e0523bc83973c0b4bd0e31b74bb8d177b369
eveporcello <eve@moonhighway.com>
2024-07-22 20:50:54 -0400

    Adds 05_01b

--- script.ts
+++ script.ts
@@ -1,45 +1,136 @@
-type HotelCategory = "gold" | "silver" | "bronze";
-
-class Hotel {
-  readonly id: string;
-  readonly name: string;
-  cost: number;
-  amenities: string[] = [];
-  category: HotelCategory;
-
-  constructor(
-    id: string,
-    name: string,
-    cost: number,
-    category: HotelCategory
-  ) {
-    this.id = id;
-    this.name = name;
-    this.cost = cost;
-    this.category = category;
-  }
-
-  addAmenity(amenity: string) {
-    this.amenities.push(amenity);
-  }
-  describeHotel(): string {
-    return `The ${this.category} category ${
-      this.name
-    } costs $${
-      this.cost
-    } and includes the following amenities: ${this.amenities.join(
-      ", "
-    )}.`;
-  }
+interface Creator {
+  firstName: string;
+  lastName: string;
+  authored?: string[];
+  created?: string[];
+  directed?: string[];
+  produced?: string[];
 }
 
-const peakLodge = new Hotel(
-  "06",
-  "Peak Lodge",
-  250,
-  "silver"
-);
-peakLodge.addAmenity("breakfast");
-peakLodge.addAmenity("wifi");
-let description = peakLodge.describeHotel();
-console.log(description);
+const authors: Creator[] = [
+  {
+    firstName: "Harper",
+    lastName: "Lee",
+    authored: ["To Kill a Mockingbird", "Go Set a Watchman"]
+  },
+  {
+    firstName: "Aldous",
+    lastName: "Huxley",
+    authored: ["Brave New World", "The Doors of Perception"]
+  }
+];
+
+const artists: Creator[] = [
+  {
+    firstName: "Pink",
+    lastName: "Floyd",
+    created: ["The Dark Side of the Moon", "The Wall"]
+  },
+  {
+    firstName: "Snoop",
+    lastName: "Dogg",
+    created: ["Tha Doggfather", "Coolaid"]
+  }
+];
+
+const directors: Creator[] = [
+  {
+    firstName: "Christopher",
+    lastName: "Nolan",
+    directed: ["Inception", "Interstellar"]
+  },
+  {
+    firstName: "Stanley",
+    lastName: "Kubrick",
+    directed: [
+      "2001: A Space Odyssey",
+      "A Clockwork Orange"
+    ]
+  }
+];
+
+const producers: Creator[] = [
+  {
+    firstName: "Emma",
+    lastName: "Thomas",
+    produced: ["Inception", "Interstellar"]
+  },
+  {
+    firstName: "Katharina",
+    lastName: "Otto-Bernstein",
+    produced: ["Absolute Wilson", "Beuys"]
+  }
+];
+
+const creators: Record<string, Creator[]> = {
+  authors,
+  artists,
+  directors,
+  producers
+};
+
+for (let creator of creators.authors) {
+  console.log(
+    `${creator.firstName} ${
+      creator.lastName
+    } is an author who wrote the books ${creator.authored.join(
+      ", "
+    )}`
+  );
+}
+
+for (let creator of creators.artists) {
+  console.log(
+    `${creator.firstName} ${
+      creator.lastName
+    } is an artist who created the albums ${creator.created.join(
+      ", "
+    )}`
+  );
+}
+
+for (let creator of creators.directors) {
+  console.log(
+    `${creator.firstName} ${
+      creator.lastName
+    } is a director who directed the movies ${creator.directed.join(
+      ", "
+    )}`
+  );
+}
+
+for (let creator of creators.producers) {
+  console.log(
+    `${creator.firstName} ${
+      creator.lastName
+    } is a producer who produced the movies ${creator.produced.join(
+      ", "
+    )}`
+  );
+}
+
+const filmCreators: Creator[] = [
+  ...directors,
+  ...producers
+];
+
+for (let creator of filmCreators) {
+  let creatorType = "<unknown>";
+  let created: string[] = [];
+
+  if (Array.isArray(creator.directed)) {
+    creatorType = "director";
+    created = creator.directed;
+  } else if (Array.isArray(creator.produced)) {
+    creatorType = "producer";
+    created = creator.produced;
+  }
+
+  console.log(
+    `${creator.firstName} ${
+      creator.lastName
+    } is a ${creatorType} who helped create ${created.join(
+      ", "
+    )}`
+  );
+}
