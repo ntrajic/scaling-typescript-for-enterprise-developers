b475db231b4f4ef0bff6c9ae5296c75cc22c5890
Nikola D Trajic <ntrajic@users.noreply.github.com>
2025-07-05 00:51:51 +0000

    Merge branch 03_06e into main under 03_06e

---
+++ 03_06e/script.js
@@ -0,0 +1,17 @@
+var oakData = {
+    name: "Oak",
+    height: 20,
+    age: 100
+};
+var oak = {
+    getName: function () { return oakData.name; },
+    getHeight: function () { return oakData.height; },
+    getAge: function () { return oakData.age; }
+};
+console.log("Name: ".concat(oak.getName()));
+console.log("Height: ".concat(oak.getHeight()));
+console.log("Age: ".concat(oak.getAge()));
+var partialOak = {
+    name: "Oak"
+};
+console.log(partialOak);
