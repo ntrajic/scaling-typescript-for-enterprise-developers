46677e8bd0fc9c17b90b8c5102571eef9d721b12
Nikola D Trajic <ntrajic@users.noreply.github.com>
2025-07-05 00:51:52 +0000

    Merge branch 04_01e into main under 04_01e

---
+++ 04_01e/city.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.formatCity = formatCity;
+function formatCity(city) {
+    return "".concat(city.name, ", ").concat(city.country);
+}
