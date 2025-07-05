336f1a1bee8c892ce7b1a6d811344d7c6e59115c
Nikola D Trajic <ntrajic@users.noreply.github.com>
2025-07-05 00:51:58 +0000

    Merge branch 05_05e into main under 05_05e

---
+++ 05_05e/script.test-d.ts
@@ -0,0 +1,27 @@
+import {
+  describe,
+  it,
+  expect,
+  assertType,
+  expectTypeOf,
+  test
+} from "vitest";
+import { printCity, type City } from "./script";
+
+describe("printCity", () => {
+  it("Should return the city name and country", () => {
+    const city: City = {
+      name: "San Francisco",
+      country: "USA"
+    };
+    const result = printCity(city);
+    expect(result).toBe("San Francisco, USA");
+  });
+});
+
+test("my types are working as expected", () => {
+  expectTypeOf(printCity).toBeFunction();
+  expectTypeOf(printCity)
+    .parameter(0)
+    .toMatchTypeOf<City>();
+});
