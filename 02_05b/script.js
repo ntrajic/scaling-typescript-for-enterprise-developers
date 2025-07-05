c4da8fdcdc0dc1463698ca9ac3f8fb6ad2b3c125
Nikola D Trajic <ntrajic@users.noreply.github.com>
2025-07-05 00:51:46 +0000

    Merge branch 02_05b into main under 02_05b

---
+++ 02_05b/script.js
@@ -0,0 +1,9 @@
+var language = null;
+language = "Mandarin";
+console.log(language);
+function welcomeToCity(city) {
+    return "Welcome to ".concat(city);
+}
+console.log(welcomeToCity("Seoul"));
+console.log(welcomeToCity("Sydney"));
+console.log(welcomeToCity("Minneapolis"));
