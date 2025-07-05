d1ff4317251765432356b375af240c97c5e6ec18
Nikola D Trajic <ntrajic@users.noreply.github.com>
2025-07-05 00:51:44 +0000

    Merge branch 02_02b into main under 02_02b

---
+++ 02_02b/script.js
@@ -0,0 +1,14 @@
+var city1 = {
+    name: "Istanbul",
+    country: "Turkey"
+};
+var city2 = {
+    name: "Amsterdam",
+    country: "Netherlands"
+};
+var venice = {
+    name: "Venice",
+    country: "Italy",
+    nickname: "The Floating City"
+};
+console.log(venice);
