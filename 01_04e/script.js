0c1faa60f1c60c3a0196d010d34ffe4a6c334902
Nikola D Trajic <ntrajic@users.noreply.github.com>
2025-07-05 00:51:41 +0000

    Merge branch 01_04e into main under 01_04e

---
+++ 01_04e/script.js
@@ -0,0 +1,12 @@
+var tokyo = {
+    name: "Tokyo",
+    population: 37400000,
+    country: "Japan",
+    capitalBuilding: "Tokyo Metropolitan Government Building",
+    landmarks: [
+        "Imperial Palace",
+        "Tokyo Tower",
+        "Tokyo Skytree"
+    ]
+};
+console.log(tokyo);
